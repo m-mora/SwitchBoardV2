fdbea2b615b72bf2b59b92ac6e444f070e28786e
Marco Mora <morafoo@gmail.com>
2024-09-13 09:34:25 -0600

    Formated the code
    Signed-off-by: Marco Mora <morafoo@gmail.com>

--- src/Display/Display.cpp
+++ src/Display/Display.cpp
@@ -1,10 +1,10 @@
 #include "Display.h"
 
-
-void Display::init(uint8_t address) 
+void Display::init(uint8_t address)
 {
     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    if(!this->begin(SSD1306_SWITCHCAPVCC, address)) { 
+    if (!this->begin(SSD1306_SWITCHCAPVCC, address))
+    {
         Serial.println(F("SSD1306 allocation failed"));
     }
 
@@ -14,81 +14,90 @@ void Display::init(uint8_t address)
     this->ShowLogo();
     delay(3000);
 
-    Serial.println(F("Display Initialized")); 
+    Serial.println(F("Display Initialized"));
 }
 
-void Display::ShowLogo() {
-    this->drawBitmap(0,0,bitmap_klic_logo,128,64,WHITE);
+void Display::ShowLogo()
+{
+    this->drawBitmap(0, 0, bitmap_klic_logo, 128, 64, WHITE);
     this->display();
 }
 
-void Display::demo(uint16_t dly) {
+void Display::demo(uint16_t dly)
+{
     this->testdrawline();
-    delay(dly/2);
+    delay(dly / 2);
     this->clearDisplay();
     this->ShowLogo();
-    delay(dly/2);
+    delay(dly / 2);
 }
 
-void Display::testdrawline() {
-  int16_t i;
+void Display::testdrawline()
+{
+    int16_t i;
 
-  clearDisplay(); // Clear display buffer
+    clearDisplay(); // Clear display buffer
 
-  for(i = 0; i < width(); i += 4) {
-    drawLine(0, 0, i, height() - 1, WHITE);
-    display(); // Update screen
-    delay(1);
-  }
-  for(i = 0; i < height(); i += 4) {
-    drawLine(0, 0, width() - 1, i, WHITE);
-    display();
-    delay(1);
-  }
-  delay(250);
+    for (i = 0; i < width(); i += 4)
+    {
+        drawLine(0, 0, i, height() - 1, WHITE);
+        display(); // Update screen
+        delay(1);
+    }
+    for (i = 0; i < height(); i += 4)
+    {
+        drawLine(0, 0, width() - 1, i, WHITE);
+        display();
+        delay(1);
+    }
+    delay(250);
 
-  clearDisplay();
+    clearDisplay();
 
-  for(i = 0; i < width(); i += 4) {
-    drawLine(0, height() - 1, i, 0, WHITE);
-    display();
-    delay(1);
-  }
-  for(i = height() - 1; i >= 0; i -= 4) {
-    drawLine(0, height() - 1, width() - 1, i, WHITE);
-    display();
-    delay(1);
-  }
-  delay(250);
+    for (i = 0; i < width(); i += 4)
+    {
+        drawLine(0, height() - 1, i, 0, WHITE);
+        display();
+        delay(1);
+    }
+    for (i = height() - 1; i >= 0; i -= 4)
+    {
+        drawLine(0, height() - 1, width() - 1, i, WHITE);
+        display();
+        delay(1);
+    }
+    delay(250);
 
-  clearDisplay();
+    clearDisplay();
 
-  for(i=width()-1; i>=0; i-=4) {
-    drawLine(width()-1, height()-1, i, 0, WHITE);
-    display();
-    delay(1);
-  }
-  for(i=height()-1; i>=0; i-=4) {
-    drawLine(width()-1, height()-1, 0, i, WHITE);
-    display();
-    delay(1);
-  }
-  delay(250);
+    for (i = width() - 1; i >= 0; i -= 4)
+    {
+        drawLine(width() - 1, height() - 1, i, 0, WHITE);
+        display();
+        delay(1);
+    }
+    for (i = height() - 1; i >= 0; i -= 4)
+    {
+        drawLine(width() - 1, height() - 1, 0, i, WHITE);
+        display();
+        delay(1);
+    }
+    delay(250);
 
-  clearDisplay();
+    clearDisplay();
 
-  for(i=0; i<height(); i+=4) {
-    drawLine(width()-1, 0, 0, i, WHITE);
-    display();
-    delay(1);
-  }
-  for(i=0; i<width(); i+=4) {
-    drawLine(width()-1, 0, i, height()-1, WHITE);
-    display();
-    delay(1);
-  }
+    for (i = 0; i < height(); i += 4)
+    {
+        drawLine(width() - 1, 0, 0, i, WHITE);
+        display();
+        delay(1);
+    }
+    for (i = 0; i < width(); i += 4)
+    {
+        drawLine(width() - 1, 0, i, height() - 1, WHITE);
+        display();
+        delay(1);
+    }
 
-  delay(2000); // Pause for 2 seconds
+    delay(2000); // Pause for 2 seconds
 }
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,18 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 
 Display display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     display.init();
 }
 
-void loop() {
+void loop()
+{
     Serial.println(F("en el loop"));
     display.demo(2000);
 }
