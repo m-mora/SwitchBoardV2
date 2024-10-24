0e523ded541637d92a181764d901524031ad1b5d
Marco Mora <morafoo@gmail.com>
2024-10-23 20:50:35 -0600

    Implement feedback, and cache the configuration to avoid many access to EEPROM
    Signed-off-by: Marco Mora <morafoo@gmail.com>

--- src/Storage/scheduleconf.h
+++ src/Storage/scheduleconf.h
@@ -1,23 +1,24 @@
 #pragma once
 
-#include <Preferences.h>
+#include <Preferences.h> // this functions are to use the EEPROM
 
-#define MAX_ZONES 4
+#define SUNDAY 1 << 6
+#define MONDAY 1 << 5
+#define TUESDAY 1 << 4
+#define WEDNESDAY 1 << 3
+#define THURSDAY 1 << 2
+#define FRIDAY 1 << 1
+#define SATURDAY 1
+#define ALLWEEK 0x7F
+
 enum
 {
     ZONE1 = 0,
     ZONE2,
     ZONE3,
-    ZONE4
+    ZONE4,
+    MAX_ZONES
 } zones_e;
-
-typedef struct
-{
-    uint8_t days;
-    uint8_t hour;
-    uint8_t minute;
-    uint16_t duration;
-} schedule_t;
 
 /*
  * Day: NA | Sun | Mon | Tues | wed | thur | Fri | Sat |
@@ -26,57 +27,28 @@ typedef struct
  * Minutes:
  * Duration: in seconds
  */
-schedule_t defaultZone1 = {0x54, 23, 0, 120};
-schedule_t defaultZone2 = {0x54, 23, 5, 120};
-schedule_t defaultZone3 = {0x54, 23, 10, 120};
-schedule_t defaultZone4 = {0x54, 23, 15, 120};
+typedef struct
+{
+    uint8_t days;
+    uint8_t hour;
+    uint8_t minute;
+    uint16_t duration;
+} schedule_t;
 
 class ScheduleConf
 {
 private:
     Preferences *store;
     String _zoneNames[MAX_ZONES] = {"zone1", "zone2", "zone3", "zone4"};
-    schedule_t _zones[MAX_ZONES] = {defaultZone1, defaultZone2, defaultZone3, defaultZone4};
+    // these are the default values if not previous configuration was done.
+    schedule_t _zones[MAX_ZONES] = {{0x54, 23, 0, 120}, {0x54, 23, 5, 120}, {0x54, 23, 10, 120}, {0x54, 23, 15, 120}};
     String _nameSpace = "schedule";
 
 public:
-    ScheduleConf() {};
-    ~ScheduleConf() {};
+    ScheduleConf();
+    ~ScheduleConf();
 
     void init(void);
     schedule_t getConf(int zone);
     void setConf(int zone, schedule_t *conf);
 };
-
-void ScheduleConf::init()
-{
-    store = new Preferences();
-    store->begin(_nameSpace.c_str(), false);
-    // Check if it is the first time it runs and there is not data saved.
-    if (!store->isKey(this->_zoneNames[0].c_str()))
-    {
-        // if not data saved, save default values
-        for (int i = 0; i < MAX_ZONES; i++)
-        {
-            store->putBytes(this->_zoneNames[i].c_str(), &this->_zones[i], sizeof(schedule_t));
-        }
-    }
-    // close the namespace in case some other code wants to save data in a different one.
-    store->end();
-}
-
-schedule_t ScheduleConf::getConf(int zone)
-{
-    schedule_t result;
-    store->begin(_nameSpace.c_str());
-    store->getBytes(this->_zoneNames[zone].c_str(), &result, sizeof(schedule_t));
-    store->end();
-    return result;
-}
-
-void ScheduleConf::setConf(int zone, schedule_t *conf)
-{
-    store->begin(_nameSpace.c_str(), false);
-    store->putBytes(this->_zoneNames[zone].c_str(), conf, sizeof(schedule_t));
-    store->end();
-}
--- test/test_storage/test_storage.cpp
+++ test/test_storage/test_storage.cpp
@@ -7,69 +7,29 @@ ScheduleConf sch;
 void test_saved_values()
 {
     schedule_t s;
+    uint8_t days[MAX_ZONES] = {(ALLWEEK), (MONDAY | WEDNESDAY | FRIDAY), (TUESDAY | THURSDAY | SATURDAY), (SUNDAY | WEDNESDAY | SATURDAY)};
+    uint8_t hour[MAX_ZONES] = {12, 21, 1, 23};
+    uint8_t minutes[MAX_ZONES] = {30, 9, 1, 0};
+    uint16_t duration[MAX_ZONES] = {600, 1000, 20, 250};
 
-    s.days = 0xEF;
-    s.hour = 12;
-    s.minute = 30;
-    s.duration = 600;
-    sch.setConf(ZONE1, &s);
-    // clear the values
-    s = {};
-    Serial.printf("Days = 0x%X, Hour = %d, Min = %d, Duration = %d\n", s.days, s.hour, s.minute, s.duration);
-    // read values back
-    s = sch.getConf(ZONE1);
-    Serial.printf("Days = 0x%X, Hour = %d, Min = %d, Duration = %d\n", s.days, s.hour, s.minute, s.duration);
-    TEST_ASSERT_EQUAL(0xEF, s.days);
-    TEST_ASSERT_EQUAL(12, s.hour);
-    TEST_ASSERT_EQUAL(30, s.minute);
-    TEST_ASSERT_EQUAL(600, s.duration);
-    s.days = 0xE0;
-    s.hour = 21;
-    s.minute = 9;
-    s.duration = 1000;
-    sch.setConf(ZONE2, &s);
-    // clear the values
-    s = {};
-    Serial.printf("Days = 0x%X, Hour = %d, Min = %d, Duration = %d\n", s.days, s.hour, s.minute, s.duration);
-    // read values back
-    s = sch.getConf(ZONE2);
-    Serial.printf("Days = 0x%X, Hour = %d, Min = %d, Duration = %d\n", s.days, s.hour, s.minute, s.duration);
-    TEST_ASSERT_EQUAL(0xE0, s.days);
-    TEST_ASSERT_EQUAL(21, s.hour);
-    TEST_ASSERT_EQUAL(9, s.minute);
-    TEST_ASSERT_EQUAL(1000, s.duration);
-
-    s.days = 0x0F;
-    s.hour = 1;
-    s.minute = 1;
-    s.duration = 20;
-    sch.setConf(ZONE3, &s);
-    // clear the values
-    s = {};
-    Serial.printf("Days = 0x%X, Hour = %d, Min = %d, Duration = %d\n", s.days, s.hour, s.minute, s.duration);
-    // read values back
-    s = sch.getConf(ZONE3);
-    Serial.printf("Days = 0x%X, Hour = %d, Min = %d, Duration = %d\n", s.days, s.hour, s.minute, s.duration);
-    TEST_ASSERT_EQUAL(0x0F, s.days);
-    TEST_ASSERT_EQUAL(1, s.hour);
-    TEST_ASSERT_EQUAL(1, s.minute);
-    TEST_ASSERT_EQUAL(20, s.duration);
-
-    s.days = 0xDD;
-    s.hour = 23;
-    s.minute = 0;
-    s.duration = 200;
-    sch.setConf(ZONE4, &s);
-    // clear the values
-    s = {};
-    Serial.printf("Days = 0x%X, Hour = %d, Min = %d, Duration = %d\n", s.days, s.hour, s.minute, s.duration);
-    // read values back
-    s = sch.getConf(ZONE4);
-    Serial.printf("Days = 0x%X, Hour = %d, Min = %d, Duration = %d\n", s.days, s.hour, s.minute, s.duration);
-    TEST_ASSERT_EQUAL(0xDD, s.days);
-    TEST_ASSERT_EQUAL(23, s.hour);
-    TEST_ASSERT_EQUAL(0, s.minute);
-    TEST_ASSERT_EQUAL(200, s.duration);
+    for (int i = ZONE1; i < MAX_ZONES; i++)
+    {
+        s.days = days[i];
+        s.hour = hour[i];
+        s.minute = minutes[i];
+        s.duration = duration[i];
+        sch.setConf(i, &s);
+        // clear the values
+        s = {};
+        Serial.printf("Zone = %d, Days = 0x%02X, Hour = %02d, Min = %02d, Duration = %d\n", i, s.days, s.hour, s.minute, s.duration);
+        // read values back
+        s = sch.getConf(i);
+        Serial.printf("Zone = %d, Days = 0x%02X, Hour = %02d, Min = %02d, Duration = %d\n", i, s.days, s.hour, s.minute, s.duration);
+        TEST_ASSERT_EQUAL(days[i], s.days);
+        TEST_ASSERT_EQUAL(hour[i], s.hour);
+        TEST_ASSERT_EQUAL(minutes[i], s.minute);
+        TEST_ASSERT_EQUAL(duration[i], s.duration);
+    }
 }
 //----------------
 void setup()
