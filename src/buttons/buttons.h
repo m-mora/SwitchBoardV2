bc197249a5260b27f3bd2ed2c228e6517d0da1f9
Marco Mora <morafoo@gmail.com>
2024-12-18 17:54:09 -0600

    draft

--- src/buttons/buttons.h
+++ src/buttons/buttons.h
@@ -1,67 +1,131 @@
 #pragma once
 
 #include <Arduino.h>
+#include <Bounce2.h>
 
 #ifndef BUTTONUP
-#define BUTTONUP 35 
+#define BUTTONUP 35
 #endif
 #ifndef BUTTONDOWN
-#define BUTTONDOWN 33 
+#define BUTTONDOWN 33
 #endif
 #ifndef BUTTONRIGHT
-#define BUTTONRIGHT 26 
+#define BUTTONRIGHT 34
 #endif
 #ifndef BUTTONLEFT
-#define BUTTONLEFT 34 
+#define BUTTONLEFT 26
 #endif
 #ifndef BUTTONENTER
-#define BUTTONENTER 14 
+#define BUTTONENTER 14
+#endif
+#ifndef DEBOUNCE_DELAY
+#define DEBOUNCE_DELAY 5 // Time in miliseconds
 #endif
 
-class Buttons
-{
-private:
-    bool currentState;
-    uint8_t pin;
-    String name;
-public:
-    Buttons(uint8_t pin, String n);
-    ~Buttons();
+Bounce2::Button button_Up = Bounce2::Button();
+Bounce2::Button button_Down = Bounce2::Button();
+Bounce2::Button button_Right = Bounce2::Button();
+Bounce2::Button button_Left = Bounce2::Button();
+Bounce2::Button button_Enter = Bounce2::Button();
 
-    void init();
-    void update();
-    bool state();
-    String getName();
-    
-};
-
-Buttons::Buttons(uint8_t pin, String n)
+void setup_buttons()
 {
-    this->name = n;
-    this->pin = pin;
-    this->currentState = LOW;
+    // Buttons setup
+    button_Up.attach(BUTTONUP, INPUT);
+    button_Down.attach(BUTTONDOWN, INPUT);
+    button_Right.attach(BUTTONRIGHT, INPUT);
+    button_Left.attach(BUTTONLEFT, INPUT);
+    button_Enter.attach(BUTTONENTER, INPUT);
+
+    // Debounce interval in miliseconds
+    button_Up.interval(DEBOUNCE_DELAY);
+    button_Down.interval(DEBOUNCE_DELAY);
+    button_Right.interval(DEBOUNCE_DELAY);
+    button_Left.interval(DEBOUNCE_DELAY);
+    button_Enter.interval(DEBOUNCE_DELAY);
+
+    // Indicate that the high state corresponds to physically pressing button
+    button_Up.setPressedState(HIGH);
+    button_Down.setPressedState(HIGH);
+    button_Right.setPressedState(HIGH);
+    button_Left.setPressedState(HIGH);
+    button_Enter.setPressedState(HIGH);
 }
 
-Buttons::~Buttons()
+void update_buttons()
 {
+    button_Up.update();
+    button_Down.update();
+    button_Right.update();
+    button_Left.update();
+    button_Enter.update();
 }
 
-void Buttons::init()
-{ 
-    pinMode(pin,INPUT);
-}
+// class Buttons
+// {
+// private:
+//     bool currentState;
+//     uint8_t pin;
+//     String name;
+//     uint8_t lastState;
+//     uint32_t lastDeboudeTime;
 
-void Buttons::update()
-{
-    currentState = digitalRead(pin);
-}
+// public:
+//     Buttons(uint8_t pin, String n);
+//     ~Buttons();
 
-String Buttons::getName()
-{
-    return this->name;
-}
+//     void init();
+//     void update();
+//     bool state();
+//     String getName();
+// };
 
-bool Buttons::state()
-{
-    return this->currentState;
-}
+// Buttons::Buttons(uint8_t pin, String n)
+// {
+//     this->name = n;
+//     this->pin = pin;
+//     this->currentState = LOW;
+//     this->lastState = LOW;
+//     this->lastDeboudeTime = 0;
+// }
+
+// Buttons::~Buttons()
+// {
+// }
+
+// void Buttons::init()
+// {
+//     pinMode(pin, INPUT);
+// }
+
+// void Buttons::update()
+// {
+//     // this->currentState = digitalRead(pin);
+//     int valueRead = digitalRead(pin);
+
+//     // Check if the button state has changed
+//     if (valueRead != this->lastState)
+//     {
+//         this->lastDeboudeTime = millis(); // Reset the debounce timer
+//     }
+
+//     // Only update the button state if the debounce time has passed
+//     if ((millis() - this->lastDeboudeTime) > DEBOUNCE_DELAY)
+//     {
+//         // Check if the button state is stable and different from the last known state
+//         if (valueRead != this->currentState)
+//         {
+//             this->currentState = valueRead;
+//         }
+//     }
+// }
+
+// String Buttons::getName()
+// {
+//     return this->name;
+// }
+
+// bool Buttons::state()
+// {
+//     return this->currentState;
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,53 @@
 #ifndef PIO_UNIT_TESTING
 #include "main.h"
 
-#ifndef SCREEN_WIDTH
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#endif
-#ifndef SCREEN_HEIGHT
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#endif
-#ifndef OLED_RESET
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#endif
-#define MAXBUTTONS  5
+// #define MAXBUTTONS 5
 
+// Buttons buttonUp(BUTTONUP, "Up");
+// Buttons buttonDown(BUTTONDOWN, "Down");
+// Buttons buttonRight(BUTTONRIGHT, "Right");
+// Buttons buttonLeft(BUTTONLEFT, "Left");
+// Buttons buttonEnter(BUTTONENTER, "Enter");
 
+// Buttons button[] = {buttonUp, buttonDown, buttonRight, buttonLeft, buttonEnter};
 
-Display display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-Buttons buttonUp(BUTTONUP,"ButtonUp");
-Buttons buttonDown(BUTTONDOWN, "ButtonDown");
-Buttons buttonRight(BUTTONRIGHT,"ButtonRight");
-Buttons buttonLeft(BUTTONLEFT, "ButtonLeft");
-Buttons buttonEnter(BUTTONENTER, "ButtonEnter");
-
-Buttons button[]={buttonUp,buttonDown,buttonRight,buttonLeft,buttonEnter};
+IDisplay *display;
 
 void setup()
 {
     Serial.begin(115200);
-    for(int index = 0 ; index < MAXBUTTONS; index++)
-    {
-        button[index].init();
-    }
+    display = new ST7735Display(SPI_CS, SPI_DC, SPI_MOSI, SPI_SCK, TFT_RESET);
+    display->clearScreen();
+    display->display();
+    setup_buttons();
+    // for (int index = 0; index < MAXBUTTONS; index++)
+    // {
+    //     button[index].init();
+    // }
 }
 
 void loop()
 {
-    for(int index = 0 ; index < MAXBUTTONS; index++)
+    update_buttons();
+
+    if (button_Up.pressed())
     {
-        button[index].update();
-        Serial.printf("Button pressed %d value %d\n",index, button[index].state());
+            display->setCursor(0,0);
+    display->setTextColor(IDISPLAY_BLUE);
+    display->setTextSize(i);
+    display->print("Text size");
+    display->print(i);
+    display->display();
     }
-    delay(1000);
+    // for (int index = 0; index < MAXBUTTONS; index++)
+    // {
+    //     button[index].update();
+    //     if (button[index].state() == HIGH)
+    //     {
+    //         Serial.printf("Button pressed %s value %d\n", button[index].getName(), button[index].state());
+    //     }
+    // }
+    delay(100);
 }
 
 #endif
